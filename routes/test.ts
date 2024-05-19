1f51e35ec1ca4a957795f8450fc31a61c083c47c
Joao Saraiva <joao.saraiva@seemly.pt>
2024-05-19 12:43:04 +0100

    Validation using hono/zod-validator

--- routes/test.ts
+++ routes/test.ts
@@ -1,3 +1,51 @@
 import { Hono } from "hono";
+import { z } from "zod";
+import { zValidator } from "@hono/zod-validator";
 
-export const testRoute = new Hono().get("/", (c) => c.json({ expenses: [] }));
+const itemSchema = z.object({
+  id: z.number().int().positive(),
+  name: z.string().min(1, "Name is required"),
+});
+
+type item = z.infer<typeof itemSchema>;
+
+type value = {
+  id: number;
+  value: number;
+  date: Date;
+};
+
+const items: item[] = [];
+
+export const testRoute = new Hono()
+  .get("/", (c) => c.json({ items }))
+  .post("/", zValidator("json", itemSchema), (c) => {
+    try {
+      const data = c.req.valid("json");
+      console.log("🚀 ~ .post ~ data:", data);
+
+      items.push(data);
+
+      console.log("🚀 ~ .post ~ items:", items);
+      return c.json({ data });
+    } catch (error) {
+      console.error("Error:", error);
+      c.status(400);
+      return c.json({ error: "Bad Request" });
+    }
+  })
+  .delete("/", zValidator("json", itemSchema), (c) => {
+    try {
+      const data = c.req.valid("json");
+      console.log("🚀 ~ .delete ~ data:", data);
+
+      items.splice(data.id, 1);
+
+      console.log("🚀 ~ .delete ~ items:", items);
+      return c.json({ data });
+    } catch (error) {
+      console.error("Error:", error);
+      c.status(400);
+      return c.json({ error: "Bad Request" });
+    }
+  });
